6fbe0f3dd9b4e06722a6a0676822ba39fd99e4e0
Suzannah Cooper <suzannah.cooper@outlook.com>
2019-12-15 20:59:07 +1000

    Finish layout and routs

--- Gemfile
+++ Gemfile
@@ -58,6 +58,8 @@ group :test do
   gem 'mini_backtrace'
   gem 'guard'
   gem 'guard-minitest'
+
+  gem 'rails-controller-testing'
 end
 
 group :production do
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   
+  get 'users/new'
   root 'static_pages#home'
 
-  get 'static_pages/home'
-  get 'static_pages/help'
+  # creates fdfjhskj_path and adjksldj _url
+  # can use as: __ at end to change what the path or url refers to
 
-  get 'static_pages/about'
+  get '/help', to: 'static_pages#help'
 
-  
-  get 'static_pages/contact'
+  get '/about', to: 'static_pages#about'
+
+  get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  
+  get '/signup', to: 'users#new', as: 'signup'
 
 end
