1ac4dce2a42e44930f60f977f1d20356a8f649d9
Suzannah Cooper <suzannah.cooper@outlook.com>
2019-12-21 17:14:09 +1000

    started setup of user page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   # creates fdfjhskj_path and adjksldj _url
   # can use as: __ at end to change what the path or url refers to
-
   get '/help', to: 'static_pages#help'
 
   get '/about', to: 'static_pages#about'
@@ -13,6 +12,8 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/signup', to: 'users#new', as: 'signup'
+  get '/signup', to: 'users#new'
+
+  resources :users
 
 end
