b9dfa1ed9270cc9ef49dec2a329f5a2a261c39d7
Suzannah Cooper <suzannah.cooper@outlook.com>
2020-01-04 19:44:22 +1000

    fixed so dropdown shows

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_17_095202) do
+ActiveRecord::Schema.define(version: 2020_01_04_013058) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
